600079c70b5bed11e7a9d7ec74bdef65a648737d
justinwongeecs <jwonghelloworld@berkeley.edu>
2023-10-27 14:50:43 -0700

    Initial Commit

---
+++ DailyJournal/DailyJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyJournalApp.swift
+//  DailyJournal
+//
+//  Created by Justin Wong on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
